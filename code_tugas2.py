9fd5d17a5122a9f5fab6244df8358dcdbbfd3126
MilakarmilaSolissa <56116982+MilakarmilaSolissa@users.noreply.github.com>
2019-10-03 23:41:04 +0900

    tugas2 pemrograman game

---
+++ code_tugas2.py
@@ -0,0 +1,8 @@
+# Tugas Ke2. MK Pemrograman Game
+
+print("Halo nama saya mila karmila solissa")
+print("angkatan 2013")
+print("Fakulatas Teknik Info UMMU Ternate")
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Info....") # baris ini dieksekui
+#Baris ini juga merupakan Baris Komentar
